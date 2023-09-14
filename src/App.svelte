357692760486408c53d0c34c85548d9961bd1bbd
Eemi Salonen <ab6905@student.jamk.fi>
2023-09-14 20:26:59 +0300

    created basic login outline

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,9 @@
 <script>
   import Login from "./Login.svelte";
+  import Header from "./Header.svelte";
 </script>
+
+<Header />
 
 <Login />
 
--- src/Login.svelte
+++ src/Login.svelte
@@ -2,6 +2,39 @@
   import Button from "./Button.svelte";
 </script>
 
-<Button>Test</Button>
+<main>
+  <div id="container">
+    <Button>Create new Tournament as guest</Button>
+    <h2>or</h2>
+    <div id="login-sign">
+      <Button>Log in</Button>
+      <Button>Sign up</Button>
+    </div>
+  </div>
+</main>
 
-<style></style>
+<style>
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+
+  #container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    height: 50%;
+  }
+
+  #login-sign {
+    display: flex;
+    flex-direction: column;
+  }
+
+  Button {
+    margin: 10px;
+  }
+</style>
