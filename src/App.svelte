6b2027b5aee10f608b7aa27945f11c587f2eea5a
Nikolas, Orava <AA3171@student.jamk.fi>
2023-09-15 13:18:10 +0300

    entiiä

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,13 @@
 <script>
   import Router from './utils/Router.svelte';
   import Header from './core/Header.svelte';
+  import Scoreboard from './core/Scoreboard.svelte';
 </script>
 
 <Header />
 <Router />
+<Scoreboard />
 
 <style>
+  
 </style>
