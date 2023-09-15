31fe665cd4ed8a265811e5665676bfae947d62a0
Nikolas, Orava <AA3171@student.jamk.fi>
2023-09-15 13:28:23 +0300

    local main

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,11 @@
 <script>
   import Router from './utils/Router.svelte';
   import Header from './core/Header.svelte';
-  import Scoreboard from './core/Scoreboard.svelte';
 </script>
 
 <Header />
 
 <Router />
-<Scoreboard />
 
 <style>
-  
 </style>
