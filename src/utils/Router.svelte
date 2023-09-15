c718a91e1662b7d2e6b4e301c79d5b74baced835
Eemi Salonen <ab6905@student.jamk.fi>
2023-09-15 11:01:07 +0300

    selection routing added

--- src/utils/Router.svelte
+++ src/utils/Router.svelte
@@ -1,12 +1,13 @@
 <script>
-  import Router from 'svelte-spa-router';
-  import Login from '../core/Login.svelte';
-  import Selection from '../core/Selection.svelte';
-  import Profile from '../core/Profile.svelte';
+  import Router from "svelte-spa-router";
+  import Login from "../core/Login.svelte";
+  import Selection from "../core/Selection.svelte";
+  import Profile from "../core/Profile.svelte";
 
   const routes = {
-    '/': Login,
-    '/profile': Profile,
+    "/": Login,
+    "/profile": Profile,
+    "/selection": Selection,
   };
 </script>
 
