754e03c470d8f03082f0d550fb7c57050dab6e58
mikkohaemaelaeinen <ab7778@student.jamk.fi>
2023-09-15 13:33:31 +0300

    footer created

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,14 @@
 <script>
   import Router from './utils/Router.svelte';
   import Header from './core/Header.svelte';
+  //import Footer from './reusable/Footer.svelte';
 </script>
 
 <Header />
 
 <Router />
+
+<!--<Footer />-->
 
 <style>
 </style>
