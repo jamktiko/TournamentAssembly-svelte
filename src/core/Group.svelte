14cc3b0a381a9be15bca5d16979d34ad3b84fe82
Eemi Salonen <ab6905@student.jamk.fi>
2023-09-15 13:58:01 +0300

    group component started

--- src/core/Group.svelte
+++ src/core/Group.svelte
@@ -1,5 +1,28 @@
-<script></script>
+<script>
+  import Button from "../reusable/Button.svelte";
 
-<main>asdf</main>
+  const groups = ["Group A", "Group B", "Group C"];
+</script>
 
-<style></style>
+<main>
+  <div id="group-manage">
+    {#each groups as group}
+      <h2>{group}</h2>
+      <Button>Click to manage group</Button>
+    {/each}
+  </div>
+</main>
+
+<style>
+  main {
+    display: grid;
+    grid-template-columns: 1fr fr 1fr;
+    grid-template-rows: auto;
+    height: 100%;
+  }
+
+  #group-manage {
+    grid-column: 1;
+    margin-left: 20px;
+  }
+</style>
