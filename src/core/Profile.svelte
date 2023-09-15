ee6918540d178f5f119440b35b50c739abf80330
Jani Haakana <aa4087@student.jamk.fi>
2023-09-15 10:03:11 +0300

    Profile page basic layout done

---
+++ src/core/Profile.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Button from '../reusable/Button.svelte';
+</script>
+
+<main>
+  <div class="button-container">
+    <div class="buttons">
+      <Button>Create A Tournament</Button>
+      <Button>My Tournaments</Button>
+    </div>
+  </div>
+</main>
+
+<style>
+  .button-container {
+    width: 75vw;
+    margin: auto;
+  }
+  .buttons {
+    display: flex;
+    justify-content: space-evenly;
+    padding: 5em;
+  }
+</style>
