16f554d5db6e3d7a559015ed1be72b70a97b979d
mikkohaemaelaeinen <ab7778@student.jamk.fi>
2023-09-15 14:24:59 +0300

    input created

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,13 @@
 <script>
   import Router from './utils/Router.svelte';
   import Header from './core/Header.svelte';
+
   //import Footer from './reusable/Footer.svelte';
 </script>
 
 <Header />
 
-<Router />
+<!--<Router />-->
 
 <!--<Footer />-->
 
