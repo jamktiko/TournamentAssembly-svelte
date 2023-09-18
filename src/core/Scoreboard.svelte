15d79e95d7a40966408a420d88fc6c7e314dcf78
Eemi Salonen <ab6905@student.jamk.fi>
2023-09-18 09:33:48 +0300

    bodyparser and cors middleware installed

--- src/core/Scoreboard.svelte
+++ src/core/Scoreboard.svelte
@@ -1,41 +1,34 @@
 <script>
-  let gridData = [
-    { name: '', columns: [''] },
-  ];
-  
+  let gridData = [{ name: "", columns: [""] }];
+
   function addRow() {
-  const numColumns = gridData[0].columns.length;
-  const newRow = {
-    name: '',
-    columns: Array(numColumns).fill('') // Create an array with the same number of empty strings as columns
-  };
-  gridData = [...gridData, newRow];
-}
+    const numColumns = gridData[0].columns.length;
+    const newRow = {
+      name: "",
+      columns: Array(numColumns).fill(""), // Create an array with the same number of empty strings as columns
+    };
+    gridData = [...gridData, newRow];
+  }
 
   function addColumn() {
     gridData.forEach((row) => {
-      row.columns.push('');
+      row.columns.push("");
     });
-    gridData = [...gridData]
+    gridData = [...gridData];
   }
-
-
 
   function removeRow(index) {
     const numRows = gridData.length;
-    if (numRows > 1){
+    if (numRows > 1) {
       gridData = gridData.filter((_, i) => i !== index);
     }
-    
-    
-
   }
 
   function removeColumn(index) {
     gridData.forEach((row) => {
       row.columns.splice(index, 1);
     });
-    gridData = [...gridData]
+    gridData = [...gridData];
   }
 
   function updateRowName(index, event) {
@@ -53,6 +46,53 @@
   }
 </script>
 
+<button on:click={addRow}>Add Row</button>
+<button on:click={addColumn}>Add Column</button>
+
+<table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      {#each gridData[0].columns as column, colIdx}
+        <th>
+          <button class="remove-button" on:click={() => removeColumn(colIdx)}
+            >X</button
+          >
+        </th>
+      {/each}
+      <th>Total</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each gridData as row, rowIdx}
+      <tr>
+        <td
+          ><input
+            type="text"
+            bind:value={row.name}
+            on:input={(event) => updateRowName(rowIdx, event)}
+          /></td
+        >
+        {#each row.columns as column, colIdx}
+          <td
+            ><input
+              type="text"
+              bind:value={column}
+              on:input={(event) => updateCellValue(rowIdx, colIdx, event)}
+            /></td
+          >
+        {/each}
+        <td>{calculateRowTotal(row)}</td>
+        <td
+          ><button class="remove-button" on:click={() => removeRow(rowIdx)}
+            >Remove</button
+          ></td
+        >
+      </tr>
+    {/each}
+  </tbody>
+</table>
+
 <style>
   input {
     color: black;
@@ -60,10 +100,10 @@
   table {
     border-collapse: collapse;
     width: 100%;
-    
   }
 
-  th, td {
+  th,
+  td {
     border: 1px solid #ccc;
     padding: 8px;
     text-align: center;
@@ -74,33 +114,3 @@
     cursor: pointer;
   }
 </style>
-
-<button on:click={addRow}>Add Row</button>
-<button on:click={addColumn}>Add Column</button>
-
-<table>
-  <thead>
-    <tr>
-      <th>Name</th>
-      {#each gridData[0].columns as column, colIdx}
-        <th>
-          <button class="remove-button" on:click={() => removeColumn(colIdx)}>X</button>
-        </th>
-      {/each}
-      <th>Total</th>
-    </tr>
-  </thead>
-  <tbody>
-    {#each gridData as row, rowIdx}
-      <tr>
-        <td><input type="text" bind:value={row.name} on:input={(event) => updateRowName(rowIdx, event)} /></td>
-        {#each row.columns as column, colIdx}
-          <td><input type="text" bind:value={column} on:input={(event) => updateCellValue(rowIdx, colIdx, event)} /></td>
-        {/each}
-        <td>{calculateRowTotal(row)}</td>
-        <td><button class="remove-button" on:click={() => removeRow(rowIdx)}>Remove</button></td>
-        
-      </tr>
-    {/each}
-  </tbody>
-</table>
