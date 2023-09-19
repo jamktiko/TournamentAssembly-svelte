9a54a57e612f223694314199146d70d4a456b197
Eemi Salonen <ab6905@student.jamk.fi>
2023-09-19 14:18:25 +0300

    backend testing

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,31 +1,20 @@
-const { MongoClient } = require('mongodb');
-require('dotenv').config({ path: '../.env' });
+const { MongoClient } = require("mongodb");
+require("dotenv").config({ path: "../.env" });
 
-class MongoConnection {
-  constructor() {
-    this._connect();
-  }
+const uri = process.env.MONGODB_URI;
 
-  async _connect() {
-    try {
-      const client = new MongoClient(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-      await client.connect();
-
-      console.log('Database connection successful!');
-
-      // You can access the MongoDB database through the 'client' object if needed.
-      // Example: const db = client.db("your_database_name");
-
-      // Close the client when you're done with it.
-      // client.close();
-    } catch (error) {
-      console.error('Database connection failed:', error);
-    }
+async function connectToMongoDB() {
+  try {
+    await client.connect();
+    console.log("Connection successful");
+  } catch {
+    console.log("Connection failed");
   }
 }
 
-module.exports = new MongoConnection();
+module.exports = { connectToMongoDB, client };
