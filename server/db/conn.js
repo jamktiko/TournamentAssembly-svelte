dcc233ab487ba853ddad0ead0241f23ce5cd08bc
Eemi Salonen <ab6905@student.jamk.fi>
2023-09-19 17:09:08 +0300

    delete redundant console.log

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,8 +1,6 @@
 const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
 
-console.log(process.env.MONGODB_URI);
-
 const client = new MongoClient(process.env.MONGODB_URI, {
   useNewUrlParser: true,
 });
