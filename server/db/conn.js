d00fdbde31d518b49760a59f20ace02ba9e4e817
mikkohaemaelaeinen <ab7778@student.jamk.fi>
2023-09-20 11:46:24 +0300

    added crud methods

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,5 +1,5 @@
-const MongoClient = require("mongodb").MongoClient;
-require("dotenv").config();
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
 
 const client = new MongoClient(process.env.MONGODB_URI, {
   useNewUrlParser: true,
@@ -8,10 +8,59 @@ const client = new MongoClient(process.env.MONGODB_URI, {
 async function connect() {
   try {
     await client.connect();
-    console.log("Connection established");
+    console.log('Connection established');
   } catch {
-    console.log("Connection failed");
+    console.log('Connection failed');
   }
 }
 
-module.exports = { connect, client };
+async function createData(text) {
+  try {
+    const db = await connectToMongoDB();
+    const result = await db.collection('data').insertOne({ text });
+    console.log('Data created successfully');
+    return result.ops[0];
+  } catch (error) {
+    console.error('Error creating data:', error);
+    throw error;
+  }
+}
+
+// Function to fetch all data
+async function getAll() {
+  try {
+    const db = await connectToMongoDB();
+    const items = await db.collection('data').find().toArray();
+    console.log('Fetched all data');
+    return items;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+}
+
+// Function to update data
+async function updateData(id, text) {
+  try {
+    const db = await connectToMongoDB();
+    await db.collection('data').findOneAndUpdate({ _id: new ObjectId(id) }, { $set: { text } });
+    console.log('Data updated successfully');
+  } catch (error) {
+    console.error('Error updating data:', error);
+    throw error;
+  }
+}
+
+// Function to delete data
+async function deleteData(id) {
+  try {
+    const db = await connectToMongoDB();
+    await db.collection('data').deleteOne({ _id: new ObjectId(id) });
+    console.log('Data deleted successfully');
+  } catch (error) {
+    console.error('Error deleting data:', error);
+    throw error;
+  }
+}
+
+module.exports = { connect, createData, getAll, updateData, deleteData, client };
