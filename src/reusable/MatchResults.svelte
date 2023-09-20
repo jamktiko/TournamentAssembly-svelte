bf967e385444978eb4e1c00413f32a3a1967cac7
Nikolas, Orava <AA3171@student.jamk.fi>
2023-09-20 13:16:09 +0300

    Initial Match Results commit

---
+++ src/reusable/MatchResults.svelte
@@ -0,0 +1,16 @@
+<script>
+const matches = [
+  {
+    contestants: [
+      { name: "test1", score: 1, win: false },
+      { name: "test2", score: 2, win: true },
+    ],
+    draw: false,
+  },
+];
+
+</script>
+<main>
+  a
+</main>
+<style></style>
