fc10072b8f231d2e6744fe9494a6e92b5cd85c0a
Jani Haakana <aa4087@student.jamk.fi>
2023-09-20 11:51:22 +0300

    card component done

---
+++ src/reusable/Card.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let background = '';
+</script>
+
+<div class="card" style="background: {background}">
+  <div class="card-header">
+    <slot name="header" />
+  </div>
+  <div class="card-body">
+    <slot name="button" />
+  </div>
+  <div class="card-footer">
+    <slot name="footer" />
+  </div>
+</div>
+
+<style>
+  .card {
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    padding: 16px;
+    margin: 16px;
+    max-width: 300px;
+  }
+
+  .card-header {
+    text-align: center;
+  }
+
+  .card-footer {
+    text-align: center;
+  }
+</style>
