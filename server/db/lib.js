04f862fab573c40b451aee8716690ae91651748f
Eemi Salonen <ab6905@student.jamk.fi>
2023-09-21 13:38:04 +0300

    login returns username

--- server/db/lib.js
+++ server/db/lib.js
@@ -1,36 +1,36 @@
-const { connect, client } = require('./conn');
-const { ObjectId } = require('mongodb');
-const bcrypt = require('bcrypt');
+const { connect, client } = require("./conn");
+const { ObjectId } = require("mongodb");
+const bcrypt = require("bcrypt");
 
 const lib = {
   async getAll() {
-    const collection = await client.db('touras').collection('users');
+    const collection = await client.db("touras").collection("users");
 
     try {
       const docs = await collection.find({}).toArray();
       return docs;
     } catch {
-      console.log('Couldn´t get documents');
+      console.log("Couldn´t get documents");
       return [];
     }
   },
 
   async loginUser(username, password) {
-    const collection = client.db('touras').collection('users');
+    const collection = client.db("touras").collection("users");
 
     // Find the user by username
     const user = await collection.findOne({ username });
     try {
       if (!user) {
-        console.error('User not found');
+        console.error("User not found");
         return {
-          msg: 'Username wrong or not found',
+          msg: "Username wrong or not found",
           success: false,
         };
       }
       console.log(user);
     } catch (error) {
-      console.error('Error finding user:', error);
+      console.error("Error finding user:", error);
       throw error;
     }
     // Compare the provided password with the stored hashed password
@@ -38,31 +38,31 @@ const lib = {
 
     if (passwordMatch) {
       // Passwords match, user is authenticated
-      return user;
+      return { username: user.username };
     } else {
-      console.error('Incorrect password');
+      console.error("Incorrect password");
       return {
-        msg: 'Incorrect password',
+        msg: "Incorrect password",
         success: false,
       };
     }
   },
 
   async registerUser(username, password) {
-    const collection = client.db('touras').collection('users');
+    const collection = client.db("touras").collection("users");
 
     // Check if the username is already taken
     const existingUser = await collection.findOne({ username });
     try {
       if (existingUser) {
-        console.error('Username already exists');
+        console.error("Username already exists");
         return {
-          msg: 'Username already exists',
+          msg: "Username already exists",
           success: false,
         };
       }
     } catch (error) {
-      console.error('Error registering user:', error);
+      console.error("Error registering user:", error);
       throw error;
     }
     // Hash the password before storing it in the database
@@ -79,26 +79,29 @@ const lib = {
 
   // Function to update a document in a collection by ID
   async updateById(id, update) {
-    const collection = client.db('touras').collection('users');
+    const collection = client.db("touras").collection("users");
 
     try {
-      await collection.findOneAndUpdate({ _id: new ObjectId(id) }, { $set: update });
-      console.log('Document updated successfully');
+      await collection.findOneAndUpdate(
+        { _id: new ObjectId(id) },
+        { $set: update }
+      );
+      console.log("Document updated successfully");
     } catch (error) {
-      console.error('Error updating document:', error);
+      console.error("Error updating document:", error);
       throw error;
     }
   },
 
   // Function to delete a document in a collection by ID
   async deleteById(id) {
-    const collection = client.db('touras').collection('users');
+    const collection = client.db("touras").collection("users");
 
     try {
       await collection.deleteOne({ _id: new ObjectId(id) });
-      console.log('Document deleted successfully', id);
+      console.log("Document deleted successfully", id);
     } catch (error) {
-      console.error('Error deleting document:', error);
+      console.error("Error deleting document:", error);
       throw error;
     }
   },
