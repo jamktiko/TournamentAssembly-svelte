62584f1d58e97736b8f080a5cee91e751c99a5b2
Eemi Salonen <ab6905@student.jamk.fi>
2023-10-10 13:35:51 +0300

    db testing

--- server/db/lib.js
+++ server/db/lib.js
@@ -4,199 +4,199 @@ const { ObjectId } = require("mongodb");
 const bcrypt = require("bcrypt");
 
 const lib = {
-	async getAll() {
-		const collection = await client.db("touras").collection("users");
+  async getAll() {
+    const collection = await client.db("touras").collection("users");
 
-		try {
-			const docs = await collection.find({}).toArray();
-			return docs;
-		} catch {
-			console.log("Couldn´t get documents");
-			return [];
-		}
-	},
+    try {
+      const docs = await collection.find({}).toArray();
+      return docs;
+    } catch {
+      console.log("Couldn´t get documents");
+      return [];
+    }
+  },
 
-	async loginUser(username, password) {
-		const collection = client.db("touras").collection("users");
+  async loginUser(username, password) {
+    const collection = client.db("touras").collection("users");
 
-		if (!username || !password) {
-			console.error("Username and password must be defined");
-			return {
-				msg: "Username and password must be defined",
-				success: false,
-			};
-		}
-		// Find the user by username
-		const user = await collection.findOne({ username });
-		try {
-			if (!user) {
-				console.error("User not found");
-				return {
-					msg: "Username wrong or not found",
-					success: false,
-				};
-			}
-		} catch (error) {
-			console.error("Error finding user:", error);
-			throw error;
-		}
-		// Compare the provided password with the stored hashed password
-		const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!username || !password) {
+      console.error("Username and password must be defined");
+      return {
+        msg: "Username and password must be defined",
+        success: false,
+      };
+    }
+    // Find the user by username
+    const user = await collection.findOne({ username });
+    try {
+      if (!user) {
+        console.error("User not found");
+        return {
+          msg: "Username wrong or not found",
+          success: false,
+        };
+      }
+    } catch (error) {
+      console.error("Error finding user:", error);
+      throw error;
+    }
+    // Compare the provided password with the stored hashed password
+    const passwordMatch = await bcrypt.compare(password, user.password);
 
-		if (passwordMatch) {
-			// Passwords match, user is authenticated
-			return {
-				id: user._id,
-				username: user.username,
-				tournaments: user.tournaments,
-				token: createToken(user.username),
-				success: true,
-				msg: "Login successfull",
-			};
-		} else {
-			console.error("Incorrect password");
-			return {
-				msg: "Incorrect password",
-				success: false,
-			};
-		}
-	},
+    if (passwordMatch) {
+      // Passwords match, user is authenticated
+      return {
+        id: user._id,
+        username: user.username,
+        tournaments: user.tournaments,
+        token: createToken(user.username),
+        success: true,
+        msg: "Login successfull",
+      };
+    } else {
+      console.error("Incorrect password");
+      return {
+        msg: "Incorrect password",
+        success: false,
+      };
+    }
+  },
 
-	async registerUser(username, password, tournaments) {
-		const collection = client.db("touras").collection("users");
+  async registerUser(username, password, tournaments) {
+    const collection = client.db("touras").collection("users");
 
-		if (!username || !password) {
-			console.error("Username or password must be defined");
-			return {
-				msg: "Username or password must be defined",
-				success: false,
-			};
-		}
+    if (!username || !password) {
+      console.error("Username or password must be defined");
+      return {
+        msg: "Username or password must be defined",
+        success: false,
+      };
+    }
 
-		// Check if the username is already taken
-		const existingUser = await collection.findOne({ username });
-		try {
-			if (existingUser) {
-				console.error("Username already exists");
-				return {
-					msg: "Username already exists",
-					success: false,
-				};
-			}
-		} catch (error) {
-			console.error("Error registering user:", error);
-			throw error;
-		}
+    // Check if the username is already taken
+    const existingUser = await collection.findOne({ username });
+    try {
+      if (existingUser) {
+        console.error("Username already exists");
+        return {
+          msg: "Username already exists",
+          success: false,
+        };
+      }
+    } catch (error) {
+      console.error("Error registering user:", error);
+      throw error;
+    }
 
-		// Hash the password before storing it in the database
-		const hashedPassword = await bcrypt.hash(password, 10);
+    // Hash the password before storing it in the database
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-		const token = createToken(username);
+    const token = createToken(username);
 
-		// Insert the new user into the database
-		await collection.insertOne({
-			username: username,
-			password: hashedPassword,
-			tournaments: tournaments,
-		});
+    // Insert the new user into the database
+    await collection.insertOne({
+      username: username,
+      password: hashedPassword,
+      tournaments: tournaments,
+    });
 
-		return { token: token, username: username, success: true };
-	},
+    return { token: token, username: username, success: true };
+  },
 
-	async addTournament(username, newTournament) {
-		const collection = client.db("touras").collection("users");
+  async addTournament(username, newTournament) {
+    const collection = client.db("touras").collection("users");
 
-		// Find the user by username
-		const user = await collection.findOne({ username: username });
-		try {
-			if (!user) {
-				console.error("User not found");
-				return {
-					msg: "User not found",
-					success: false,
-				};
-			}
-		} catch (error) {
-			console.error("Error finding user:", error);
-			return {
-				msg: "Error finding user",
-				success: false,
-			};
-		}
-		// Add newTournamentData to the tournament array
+    // Find the user by username
+    const user = await collection.findOne({ username: username });
+    try {
+      if (!user) {
+        console.error("User not found");
+        return {
+          msg: "User not found",
+          success: false,
+        };
+      }
+    } catch (error) {
+      console.error("Error finding user:", error);
+      return {
+        msg: "Error finding user",
+        success: false,
+      };
+    }
+    // Add newTournamentData to the tournament array
 
-		user.tournaments.push(newTournament);
+    user.tournaments.push(newTournament);
 
-		// Update the user's document in the database
-		const updateResult = await collection.updateOne(
-			{ username: username },
-			{ $set: { tournaments: user.tournaments } }
-		);
+    // Update the user's document in the database
+    const updateResult = await collection.updateOne(
+      { username: username },
+      { $set: { tournaments: user.tournaments } }
+    );
 
-		return { success: true, result: updateResult };
-	},
+    return { success: true, result: updateResult };
+  },
 
-	async delTournament(username, delData) {
-		const collection = client.db("touras").collection("users");
+  async delTournament(username, delData) {
+    const collection = client.db("touras").collection("users");
 
-		// Find the user by username
-		const user = await collection.findOne({ username: username });
-		try {
-			if (!user) {
-				console.error("User not found");
-				return {
-					msg: "User not found",
-					success: false,
-				};
-			}
-		} catch (error) {
-			console.error("Error finding user:", error);
-			throw error;
-		}
+    // Find the user by username
+    const user = await collection.findOne({ username: username });
+    try {
+      if (!user) {
+        console.error("User not found");
+        return {
+          msg: "User not found",
+          success: false,
+        };
+      }
+    } catch (error) {
+      console.error("Error finding user:", error);
+      throw error;
+    }
 
-		// Update the user's document in the database
-		const updateResult = await collection.updateOne(
-			{ username: username },
-			{ $pull: { tournament: delData } }
-		);
+    // Update the user's document in the database
+    const updateResult = await collection.updateOne(
+      { username: username },
+      { $pull: { tournament: delData } }
+    );
 
-		return updateResult;
-	},
+    return updateResult;
+  },
 
-	// Function to update a document in a collection by ID
-	async updateById(id, update) {
-		const collection = client.db("touras").collection("users");
+  // Function to update a document in a collection by ID
+  async updateById(id, update) {
+    const collection = client.db("touras").collection("users");
 
-		try {
-			await collection.findOneAndUpdate(
-				{ _id: new ObjectId(id) },
-				{ $set: update }
-			);
-			console.log("Document updated successfully");
-		} catch (error) {
-			console.error("Error updating document:", error);
-			throw error;
-		}
-	},
+    try {
+      await collection.findOneAndUpdate(
+        { _id: new ObjectId(id) },
+        { $set: update }
+      );
+      console.log("Document updated successfully");
+    } catch (error) {
+      console.error("Error updating document:", error);
+      throw error;
+    }
+  },
 
-	// Function to delete a document in a collection by ID
-	async deleteById(id) {
-		const collection = client.db("touras").collection("users");
+  // Function to delete a document in a collection by ID
+  async deleteById(id) {
+    const collection = client.db("touras").collection("users");
 
-		try {
-			await collection.deleteOne({ _id: new ObjectId(id) });
-			console.log("Document deleted successfully", id);
-			return {
-				msg: "Document deleted successfully",
-				success: true,
-			};
-		} catch (error) {
-			console.error("Error deleting document:", error);
-			return {
-				msg: "Document deletion failed",
-				success: false,
-			};
-		}
-	},
+    try {
+      await collection.deleteOne({ _id: new ObjectId(id) });
+      console.log("Document deleted successfully", id);
+      return {
+        msg: "Document deleted successfully",
+        success: true,
+      };
+    } catch (error) {
+      console.error("Error deleting document:", error);
+      return {
+        msg: "Document deletion failed",
+        success: false,
+      };
+    }
+  },
 };
 module.exports = lib;
