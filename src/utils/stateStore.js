c08fc47a18919fda309d9a47030cbb724843ac1a
Eemi Salonen <ab6905@student.jamk.fi>
2023-10-25 09:53:48 +0300

    user tournaments component created

--- src/utils/stateStore.js
+++ src/utils/stateStore.js
@@ -4,89 +4,88 @@ import { calcId } from "./lib";
 const stateStore = writable({});
 
 const stateController = {
-	apiUrl: "http://localhost:3000/",
-	subscribe: stateStore.subscribe,
+  apiUrl: "http://localhost:3000/",
+  subscribe: stateStore.subscribe,
 
-	loginAsGuest() {
-		const guest = {
-			username: "guest",
-			password: null,
-			isGuest: true,
-			tournamentData: null,
-		};
-		stateStore.set(guest);
-	},
+  loginAsGuest() {
+    const guest = {
+      username: "guest",
+      password: null,
+      isGuest: true,
+      tournamentData: null,
+    };
+    stateStore.set(guest);
+  },
 
-	async customFetch(urlExt, opt) {
-		const res = await fetch(this.apiUrl + urlExt, opt);
-		const data = await res.json();
+  async customFetch(urlExt, opt) {
+    const res = await fetch(this.apiUrl + urlExt, opt);
+    const data = await res.json();
 
-		return data;
-	},
+    return data;
+  },
 
-	async register(user) {
-		const res = await this.customFetch("register", {
-			method: "POST",
-			mode: "cors",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(user),
-		});
-		console.log(res);
-		return res;
-	},
+  async register(user) {
+    const res = await this.customFetch("register", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    console.log(res);
+    return res;
+  },
 
-	async login(user) {
-		const res = await this.customFetch("login", {
-			method: "POST",
-			mode: "cors",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(user),
-		});
-		console.log(res);
+  async login(user) {
+    const res = await this.customFetch("login", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    console.log(res);
 
-		if (res.success) {
-			const userData = {
-				id: res.id,
-				username: res.username,
-				token: res.token,
-				tournaments: res.tournaments,
-			};
+    if (res.success) {
+      const userData = {
+        id: res.id,
+        username: res.username,
+        token: res.token,
+        tournaments: res.tournaments,
+      };
 
-			stateStore.set(userData);
-		}
+      stateStore.set(userData);
+    }
+    return res;
+  },
 
-		return res;
-	},
+  async createTournament(tournament) {
+    let user;
+    const unsub = this.subscribe((userData) => (user = userData));
+    unsub();
 
-	async createTournament(tournament) {
-		let user;
-		const unsub = this.subscribe((userData) => (user = userData));
-		unsub();
+    tournament.id = calcId(user.tournaments);
 
-		tournament.id = calcId(user.tournaments);
+    const tourData = {
+      username: user.username,
+      tournament,
+      token: user.token,
+    };
 
-		const tourData = {
-			username: user.username,
-			tournament,
-			token: user.token,
-		};
+    const opt = {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(tourData),
+    };
 
-		const opt = {
-			method: "POST",
-			mode: "cors",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(tourData),
-		};
-
-		const res = await this.customFetch("addTour", opt);
-		console.log(res);
-	},
+    const res = await this.customFetch("addTour", opt);
+    console.log(res);
+  },
 };
 
 export default stateController;
