540916f4a30eb7c224eaa1a727127306ec105049
Jani Haakana <aa4087@student.jamk.fi>
2023-10-25 10:18:09 +0300

    basic styling for tournaments.svelte

--- src/core/Tournaments.svelte
+++ src/core/Tournaments.svelte
@@ -1,6 +1,6 @@
 <script>
-  import { onDestroy } from "svelte";
-  import { stateController } from "../utils/stateStore.js";
+  import { onDestroy } from 'svelte';
+  import stateController from '../utils/stateStore.js';
 
   let user;
   const unsub = stateController.subscribe((userData) => (user = userData));
@@ -12,4 +12,34 @@
   console.log(user);
 </script>
 
-<main />
+<main>
+  <div class="tournaments-header">
+    <h1>Tournaments</h1>
+  </div>
+  <div class="tournaments-description">
+    <p>Below is a list of your created Tournaments</p>
+  </div>
+</main>
+
+<style>
+  main {
+    margin-left: 15%;
+    margin-top: 2em;
+    margin-bottom: 2em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 100%;
+    width: 70%;
+    padding-top: 4em;
+    padding-bottom: 3em;
+    border-radius: 40px;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+
+  h1 {
+    text-transform: uppercase;
+    font-size: 2em;
+  }
+</style>
