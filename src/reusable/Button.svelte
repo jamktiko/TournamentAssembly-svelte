0b01b1f70a8ed6d1d67f91269d778ff7344bff66
Jani Haakana <aa4087@student.jamk.fi>
2023-10-26 11:02:53 +0300

    user profile styling, my tournaments page styling and profile button

--- src/reusable/Button.svelte
+++ src/reusable/Button.svelte
@@ -428,4 +428,16 @@
   .league-resolve-button:disabled {
     cursor: default;
   }
+
+  .profile-button {
+    display: flex;
+    flex-direction: row;
+    text-transform: uppercase;
+    scale: 1;
+    border-radius: 10px;
+    position: absolute;
+    margin: 0.5em;
+    transition-duration: 0.1s;
+    right: 0;
+  }
 </style>
