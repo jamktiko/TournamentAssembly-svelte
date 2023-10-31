463f516a653269913de48bfc5d8c82056e95ce4e
Eemi Salonen <ab6905@student.jamk.fi>
2023-10-31 11:42:05 +0200

    commented out email validation

--- server/db/auth.js
+++ server/db/auth.js
@@ -1,5 +1,5 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 // Generate a token for the user
 function createToken(username) {
@@ -15,14 +15,14 @@ function createToken(username) {
 }
 
 function verifyToken(req, res, next) {
-  const token = req.body.token || req.headers['x-access-token'];
+  const token = req.body.token || req.headers["x-access-token"];
   if (token) {
     // verify tutkii tokenin voimassaolon ja salausmuuttujan
     jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
       if (err) {
         return res.json({
           success: false,
-          msg: 'Token is invalid or expired.',
+          msg: "Token is invalid or expired.",
         });
       } else {
         // Tallennetaan dekoodattu token request-olioon josta sitä voi jatkossa pyytää
@@ -34,43 +34,43 @@ function verifyToken(req, res, next) {
     // if there is no token, it prompts an error
     return res.status(403).send({
       success: false,
-      msg: 'Token does not exist',
+      msg: "Token does not exist",
     });
   }
 }
 
-const transporter = nodemailer.createTransport({
-  pool: true,
-  host: 'smtp.gmail.com',
-  port: 25,
-  secure: false,
-  auth: {
-    user: 'myemail@pers.com', // Change to your email and password
-    pass: 'grfj zzym odoj xens',
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   pool: true,
+//   host: 'smtp.gmail.com',
+//   port: 25,
+//   secure: false,
+//   auth: {
+//     user: 'myemail@pers.com', // Change to your email and password
+//     pass: 'grfj zzym odoj xens',
+//   },
+//   tls: {
+//     rejectUnauthorized: false,
+//   },
+// });
 
-function sendConfirmationEmail(email, confirmationToken) {
-  const confirmationLink = `http://yourwebsite.com/confirm?token=${confirmationToken}`;
-  const mailOptions = {
-    from: 'touras.official@email.com',
-    to: email,
-    subject: 'Email Confirmation',
-    text: `Click this link to confirm your email: ${confirmationLink}`,
-  };
+// function sendConfirmationEmail(email, confirmationToken) {
+//   const confirmationLink = `http://yourwebsite.com/confirm?token=${confirmationToken}`;
+//   const mailOptions = {
+//     from: 'touras.official@email.com',
+//     to: email,
+//     subject: 'Email Confirmation',
+//     text: `Click this link to confirm your email: ${confirmationLink}`,
+//   };
 
-  return new Promise((resolve, reject) => {
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(info);
-      }
-    });
-  });
-}
+//   return new Promise((resolve, reject) => {
+//     transporter.sendMail(mailOptions, (error, info) => {
+//       if (error) {
+//         reject(error);
+//       } else {
+//         resolve(info);
+//       }
+//     });
+//   });
+// }
 
-module.exports = { createToken, verifyToken, sendConfirmationEmail };
+module.exports = { createToken, verifyToken };
