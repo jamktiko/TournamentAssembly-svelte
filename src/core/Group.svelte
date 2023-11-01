1d4a9acd644484bd3f169d1e94559a600066966f
Nikolas, Orava <AA3171@student.jamk.fi>
2023-11-01 09:39:02 +0200

    Reset button for group and league

--- src/core/Group.svelte
+++ src/core/Group.svelte
@@ -394,6 +394,19 @@
   function toggleTooltip() {
     showTooltip = !showTooltip;
   }
+  function resetscore(){
+    let a = 0
+    while ( a < selected.participants.length){
+      selected.participants[a].draws = 0
+      selected.participants[a].goalDiff = 0
+      selected.participants[a].losses = 0
+      selected.participants[a].playedMatches = 0
+      selected.participants[a].score = 0
+      selected.participants[a].wins = 0
+    a += 1
+    }
+  }
+  console.log(groups)
 </script>
 
 <main>
@@ -516,6 +529,11 @@
                 disabled={agmatches.length == 0}
                 on:cClick={toggleMatches}>Show schedule</Button
               >
+              <Button
+              class="resolve-button"
+        
+              on:cClick={resetscore}>reset</Button
+            >
             {/if}
           </div>
         </div>
