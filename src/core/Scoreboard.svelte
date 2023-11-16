8800b385df4c8071526c21449a8347daa501a445
Eemi Salonen <ab6905@student.jamk.fi>
2023-11-16 11:57:37 +0200

    scoreboard saving updated

--- src/core/Scoreboard.svelte
+++ src/core/Scoreboard.svelte
@@ -1,36 +1,35 @@
 <script>
-  import { push } from 'svelte-spa-router';
-  import Button from '../reusable/Button.svelte';
-  import { slide } from 'svelte/transition';
-  import { fade } from 'svelte/transition';
-  import { scale } from 'svelte/transition';
-  import { quintOut } from 'svelte/easing';
+  import { push } from "svelte-spa-router";
+  import Button from "../reusable/Button.svelte";
+  import { slide } from "svelte/transition";
+  import { fade } from "svelte/transition";
+  import { scale } from "svelte/transition";
+  import { quintOut } from "svelte/easing";
 
-  import { onDestroy } from 'svelte';
-  import cch from '../utils/cache';
+  import { onDestroy } from "svelte";
+  import cch from "../utils/cache";
 
-  import stateController from '../utils/stateStore';
+  import stateController from "../utils/stateStore";
 
-  import { loadFromSession } from '../utils/lib';
+  import { loadFromSession } from "../utils/lib";
 
   let user;
   const unsub = stateController.subscribe((userData) => (user = userData));
 
-  if (!user.username && window.sessionStorage.getItem('user')) {
-    user = loadFromSession('user');
+  if (!user.username && window.sessionStorage.getItem("user")) {
+    user = loadFromSession("user");
     stateController.set(user);
   }
 
-  let gridData = [{ name: '', columns: [''] }];
+  let gridData = [{ name: "", columns: [""] }];
 
   const intervalId = setInterval(() => {
     if (
       (gridData[0].name && gridData[0].columns[0]) ||
       gridData[0].columns[0] === 0
     ) {
-      cch.saveToCache('scoreboard', gridData);
+      cch.saveToCache("scoreboard", gridData);
     }
-    console.log('cached');
   }, 10000);
 
   onDestroy(() => {
@@ -40,8 +39,8 @@
     if (user.state) delete user.state;
   });
 
-  if (cch.isInCache('scoreboard')) {
-    const cachedData = cch.getFromCache('scoreboard');
+  if (cch.isInCache("scoreboard")) {
+    const cachedData = cch.getFromCache("scoreboard");
     if (!Array.isArray(cachedData[0].columns)) {
       for (let unit of cachedData) {
         unit.columns = [unit.columns];
@@ -55,15 +54,15 @@
   function addRow() {
     const numColumns = gridData[0].columns.length;
     const newRow = {
-      name: '',
-      columns: Array(numColumns).fill(''), // Create an array with the same number of empty strings as columns
+      name: "",
+      columns: Array(numColumns).fill(""), // Create an array with the same number of empty strings as columns
     };
     gridData = [...gridData, newRow];
   }
 
   function addColumn() {
     gridData.forEach((row) => {
-      row.columns.push('');
+      row.columns.push("");
     });
     gridData = [...gridData];
   }
@@ -101,10 +100,10 @@
   }
 
   async function save() {
+    console.log(user.config.id, "id");
     const res = await stateController.updateTourState(
       gridData,
       user.config.id,
-      user.username
     );
 
     console.log(res);
@@ -271,7 +270,7 @@
   }
 
   /* Firefox */
-  input[type='number'] {
+  input[type="number"] {
     -moz-appearance: textfield;
     appearance: textfield;
   }
