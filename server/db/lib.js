51128de178c1566f5cab9568d39b74979870cde5
Eemi Salonen <ab6905@student.jamk.fi>
2023-11-27 11:58:36 +0200

    login messages changed

--- server/db/lib.js
+++ server/db/lib.js
@@ -86,27 +86,16 @@ const lib = {
         success: false,
       };
     }*/
-<<<<<<< HEAD
 
-    // Check for offensive words in the username
-    if (filter.isProfane(username)) {
-      console.error("Username contains inappropriate language");
-      return {
-        msg: "Username contains inappropriate language",
-=======
-    
     // Check if the username is too short
     if (username.length < 4) {
-      console.error('Username must be at least 4 characters');
+      console.error("Username must be at least 4 characters");
       return {
-        msg: 'Username is too short (minimum length is 4 characters)',
->>>>>>> fb90664c9f0208acaff8f723ae86d444903ea0b0
+        msg: "Username is too short (minimum length is 4 characters)",
         success: false,
       };
     }
 
-<<<<<<< HEAD
-=======
     // Check for offensive words in the username
     if (filter.isProfane(username)) {
       console.error("Username contains inappropriate language");
@@ -116,7 +105,6 @@ const lib = {
       };
     }
 
->>>>>>> fb90664c9f0208acaff8f723ae86d444903ea0b0
     // Check if the username is already taken
     const existingUser = await User.findOne({ username });
     try {
